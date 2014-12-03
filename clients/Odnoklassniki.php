1c8b0bcc5fd870b031ef99b21d93f4162b47a498
zckri <joker7789@gmail.com>
2014-12-03 23:51:41 +0100

    Create Odnoklassniki.php

---
+++ clients/Odnoklassniki.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace common\components\clients;
+
+use yii\authclient\OAuth2;
+use yii\helpers\Url;
+
+/**
+ * Odnoklassniki OAuth2 client 
+ *
+ * In order to use OK OAuth you must register your application at odnoklassniki.ru
+ *
+ * Example application configuration:
+ *
+ * ~~~
+ * 'components' => [
+ *     'authClientCollection' => [
+ *         'class' => 'yii\authclient\Collection',
+ *         'clients' => [
+ *               'odnoklassniki' => [
+ *                   'class' => 'common\components\clients\OdnoklassnikiClient',
+ *                   'clientId' => '{Application ID}',
+ *                   'clientSecret' => '{Secret Key}',
+ *                   'application_key' => '{Public Key}',
+ *               ],
+ *         ],
+ *     ]
+ *     ...
+ * ]
+ * ~~~
+ *
+ * @author zckri <joker7789@gmail.com>
+ */
+class Odnoklassniki extends OAuth2
+{
+    public $application_key;
+    /**
+     * @inheritdoc
+     */
+    public $authUrl = 'http://www.odnoklassniki.ru/oauth/authorize';
+    /**
+     * @inheritdoc
+     */
+    public $tokenUrl = 'https://api.odnoklassniki.ru/oauth/token.do';
+    /**
+     * @inheritdoc
+     */
+    public $apiBaseUrl = 'http://api.odnoklassniki.ru';
+    /**
+     * @inheritdoc
+     */
+    public $scope = 'VALUABLE_ACCESS';
+
+    /**
+     * @inheritdoc
+     */
+    protected function initUserAttributes()
+    {
+        return $this->api('users.getCurrentUser', 'GET');
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function apiInternal($accessToken, $url, $method, array $params, array $headers)
+    {
+        $params['access_token'] = $accessToken->getToken();
+        $params['application_key'] = $this->application_key;
+
+        $a = 'application_key='.$this->application_key.'method='.$url;
+        $b = md5($params['access_token'].$this->clientSecret);
+        $c = md5($a.$b);
+        $params['method'] = $url;
+        $params['sig'] = $c;
+
+        return $this->sendRequest($method, $url, $params, $headers);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function defaultName()
+    {
+        return 'odnoklassniki';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function defaultTitle()
+    {
+        return 'Odnoklassniki';
+    }
+}

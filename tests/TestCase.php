3fa3809e1d48238a49269c231a6c4afd16f8f462
Carsten Brandt <mail@cebe.cc>
2015-03-23 15:05:34 +0100

    added authclient unit tests

---
+++ tests/TestCase.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace yiiunit\extensions\authclient;
+
+use yii\di\Container;
+use yii\helpers\ArrayHelper;
+use Yii;
+
+/**
+ * This is the base class for all yii framework unit tests.
+ */
+abstract class TestCase extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Clean up after test.
+     * By default the application created with [[mockApplication]] will be destroyed.
+     */
+    protected function tearDown()
+    {
+        parent::tearDown();
+        $this->destroyApplication();
+    }
+
+    /**
+     * Populates Yii::$app with a new application
+     * The application will be destroyed on tearDown() automatically.
+     * @param array $config The application configuration, if needed
+     * @param string $appClass name of the application class to create
+     */
+    protected function mockApplication($config = [], $appClass = '\yii\console\Application')
+    {
+        new $appClass(ArrayHelper::merge([
+            'id' => 'testapp',
+            'basePath' => __DIR__,
+            'vendorPath' => dirname(__DIR__) . '/vendor',
+        ], $config));
+    }
+
+    protected function mockWebApplication($config = [], $appClass = '\yii\web\Application')
+    {
+        new $appClass(ArrayHelper::merge([
+            'id' => 'testapp',
+            'basePath' => __DIR__,
+            'vendorPath' => dirname(__DIR__) . '/vendor',
+            'components' => [
+                'request' => [
+                    'cookieValidationKey' => 'wefJDF8sfdsfSDefwqdxj9oq',
+                    'scriptFile' => __DIR__ .'/index.php',
+                    'scriptUrl' => '/index.php',
+                ],
+            ]
+        ], $config));
+    }
+
+    /**
+     * Destroys application in Yii::$app by setting it to null.
+     */
+    protected function destroyApplication()
+    {
+        Yii::$app = null;
+        Yii::$container = new Container();
+    }
+}

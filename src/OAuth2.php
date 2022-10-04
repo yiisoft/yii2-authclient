004da05090e6d7e214f746f7928f7d009150a916
Vito <v.neufeld@duschservice.de>
2022-10-04 10:55:17 +0200

    Fix #351: Unable to set TokenParamKey in OAuth2 config, gets hard overwritten in OAuth2::createToken()

--- src/OAuth2.php
+++ src/OAuth2.php
@@ -226,7 +226,8 @@ abstract class OAuth2 extends BaseOAuth
      */
     protected function createToken(array $tokenConfig = [])
     {
-        $tokenConfig['tokenParamKey'] = 'access_token';
+        $defaultTokenConfig = ['tokenParamKey' => 'access_token'];
+        $tokenConfig = array_merge($defaultTokenConfig, $tokenConfig);
 
         return parent::createToken($tokenConfig);
     }
